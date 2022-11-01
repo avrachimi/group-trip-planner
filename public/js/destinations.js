ff42e8d5fe47e0b427f95b91113da23b7e1767aa
Andreas Vrachimi <avrachimi@hotmail.com>
2022-11-01 20:07:50 +0000

    Added Destinations page, implemented mobile navbar

--- public/js/destinations.js
+++ public/js/destinations.js
@@ -1,3 +1,16 @@
+const openMenu = document.getElementById('mobile-open-menu');
+const exitMenu = document.getElementById('mobile-exit-menu');
+const nav = document.querySelector('nav');
+
+openMenu.addEventListener('click', () => {
+    nav.classList.add('show-menu');
+});
+
+exitMenu.addEventListener('click', () => {
+    nav.classList.remove('show-menu');
+});
+
+
 function viewDestination(id) {
     window.location.href = `/destinations/${id}`;
 }
